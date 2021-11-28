0e78e3c557f0e6943ca8a99c46afb150edca9249
oussama <bz.oussama@gmail.com>
2021-11-28 19:39:58 +0100

    initial experience page

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" className="dark">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Merriweather:wght@400;700&family=Montserrat:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="overflow-x-hidden bg-dark-500 text-whitetxt">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      kaushan: ['Kaushan Script', 'cursive'],
+      lines: ['Merriweather', 'Georgia', 'serif'],
+      headers: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: '#00FFFF',
+        },
+        dark: {
+          DEFAULT: '#010101',
+          100: '#0a0b0e',
+          200: '#16181d',
+          300: '#16181d',
+          500: '#282c35',
+          700: '#363c48',
+        },
+        whitetxt: {
+          DEFAULT: 'rgba(255, 255, 255, 0.88)',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
