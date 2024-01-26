1dccdb67f800c8975a8194ca0a7daae665ec5a19
Oussama <oussama@notifyai.io>
2024-01-26 13:33:22 +0400

    house keeping

--- pages/index.tsx
+++ pages/index.tsx
@@ -268,24 +268,7 @@ const Home: NextPage = () => {
       <Skills />
       <Technologies />
       <Footer />
-      <Script
-        id="notifyaiscr"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(document, window) {
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.src = "https://secureanalytic.com/scripts/push/script/64d5pjjgj0?url=" + encodeURI(self.location.hostname) + "&alturl=" + encodeURI(self.location.pathname);
-            script.onload = function() {
-            push_init();
-            push_subscribe();
-            };
-           document.getElementsByTagName("head")[0].appendChild(script);
-           })(document, window);
-      `,
-        }}
-      />
+      
     </>
   );
 };
--- sections/Experience.tsx
+++ sections/Experience.tsx
@@ -1,21 +1,156 @@
-import { NextPage } from 'next';
-import Achievement from './components/achievement';
-import TechsInfo from '../data/techsinfo';
+import { NextPage } from "next";
+import Achievement from "./components/achievement";
+import TechsInfo from "../data/techsinfo";
 
 const Experience: NextPage = () => {
   return (
     <section
       className="mx-auto overflow-hidden bg-white font-lines"
-      style={{ background: '#fefefe' }}
+      style={{ background: "#fefefe" }}
     >
       <div className="px-8 text-center">
         <h2
           id="experience"
-          className="py-8 text-4xl font-bold tracking-wider text-center font-headers"
+          className="py-8 mt-8 text-4xl font-bold tracking-wider text-center font-headers"
         >
           Professional Experience
         </h2>
+      </div>
 
+      {/* ADWORA */}
+      <div className="px-8 text-center">
+        <h3 className="mt-2 text-3xl font-bold tracking-wider font-headers ">
+          Software Engineer
+        </h3>
+        <div className="flex flex-col items-center justify-center max-w-lg mx-auto mb-6 md:flex-row">
+          <a
+            href="https://adwora.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="block font-bold tracking-widest cursor-pointer font-lines 3xl:text-lg"
+          >
+            <span className="pr-8 italic font-bold tracking-wide text-md">
+              • Aug 2023 - Today
+            </span>
+          </a>
+          <a
+            href="https://adwora.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="tracking-wider 3xl:text-lg"
+          >
+            @Adwora
+          </a>
+        </div>
+        <p className="max-w-lg pb-4 mx-auto text-lg text-center 3xl:max-w-2xl">
+          After delivering the lead gen system{" "}
+          <a href="#notifyai" className="text-sm">
+            @NotifyAI
+          </a>{" "}
+          , We started a brand new media buying solution that connects
+          advertisers with publishers to reach their target audience
+          effectively.
+        </p>
+      </div>
+
+      <Achievement
+        direction="right"
+        imgSrc="adwora-dashboard.png"
+        imgLabel="NotifyAI Push Notification"
+        tech={[
+          TechsInfo.node,
+          TechsInfo.angular,
+          TechsInfo.sqlserver,
+          TechsInfo.websocket,
+          TechsInfo.aws,
+        ]}
+      >
+        <h3 className="text-xl font-bold tracking-wide text-center font-headers ">
+          Push Notification to Lead Gen
+        </h3>
+        <ul className="text-lg tracking-wide list-disc list-inside font-lines ">
+          <li className="mt-4">
+            I Joined the dev team at NotifyAI to leverage the Push Notification
+            System to build and automate dynamic lead generations funnels and
+            Co-Reg offers.
+          </li>
+          <li className="mt-4">
+            Once we started converting first subscriptions to actual leads with
+            user information, geo, and interest. We helped our customers to
+            narrow audience targeting and get higher ROI and cheaper CPC and
+            increased CTR
+          </li>
+        </ul>
+      </Achievement>
+
+      {/* NOTIFYAI */}
+      <div className="px-8 text-center">
+        <h3
+          id="notifyai"
+          className="mt-2 text-3xl font-bold tracking-wider font-headers "
+        >
+          Software Developer
+        </h3>
+        <div className="flex flex-col items-center justify-center max-w-lg mx-auto mb-6 md:flex-row">
+          <a
+            href="https://notifyai.io/"
+            target="_blank"
+            rel="noreferrer"
+            className="block font-bold tracking-widest cursor-pointer font-lines 3xl:text-lg"
+          >
+            <span className="pr-8 italic font-bold tracking-wide text-md">
+              • Jan - Aug 2022
+            </span>
+          </a>
+          <a
+            href="https://notifyai.io/"
+            target="_blank"
+            rel="noreferrer"
+            className="tracking-wider 3xl:text-lg"
+          >
+            @NotifyAI
+          </a>
+        </div>
+        <p className="max-w-lg pb-4 mx-auto text-lg text-center 3xl:max-w-2xl">
+          In addition to my developer skills, My Ad-Tech background, marketing
+          experience, and funnel building skills, I&apos;ve joined NotifyAI Dev
+          Team to build lead generation system. that added a new section to
+          company assets
+        </p>
+      </div>
+      {/* ----------Notify App------------------------------- */}
+      <Achievement
+        direction="left"
+        imgSrc="push-system.webp"
+        imgLabel="NotifyAI Push Notification"
+        tech={[
+          TechsInfo.node,
+          TechsInfo.angular,
+          TechsInfo.sqlserver,
+          TechsInfo.websocket,
+          TechsInfo.aws,
+        ]}
+      >
+        <h3 className="text-xl font-bold tracking-wide text-center font-headers ">
+          Push Notification to Lead Gen
+        </h3>
+        <ul className="text-lg tracking-wide list-disc list-inside font-lines ">
+          <li className="mt-4">
+            I Joined the dev team at NotifyAI to leverage the Push Notification
+            System to build and automate dynamic lead generations funnels and
+            Co-Reg offers.
+          </li>
+          <li className="mt-4">
+            Once we started converting first subscriptions to actual leads with
+            user information, geo, and interest. We helped our customers to
+            narrow audience targeting and get higher ROI and cheaper CPC and
+            increased CTR
+          </li>
+        </ul>
+      </Achievement>
+
+      {/* PHARMAINVEST */}
+      <div className="px-8 text-center mt-8">
         <h3 className="mt-2 text-3xl font-bold tracking-wider font-headers ">
           Software Developer
         </h3>
@@ -27,7 +162,7 @@ const Experience: NextPage = () => {
             className="block font-bold tracking-widest cursor-pointer font-lines 3xl:text-lg"
           >
             <span className="pr-8 italic font-bold tracking-wide text-md">
-              • 2018 - Today
+              • 2018 - 2022
             </span>
           </a>
           <a
@@ -39,13 +174,12 @@ const Experience: NextPage = () => {
             @PharmaInvest SPA
           </a>
         </div>
-        <p className="max-w-lg pb-4 mx-auto text-xl text-center 3xl:max-w-2xl">
+        <p className="max-w-lg pb-4 mx-auto text-lg text-center 3xl:max-w-2xl">
           As the developers team leader, I am responsible on creating,
           maintaining, and promoting the use of digital solutions to help the
           company on its mission towards Digital transformation.
         </p>
       </div>
-
       {/* ----------Mobile App------------------------------- */}
       <Achievement
         direction="left"
